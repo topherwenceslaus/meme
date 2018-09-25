6c91734620e70d7564c60c92d9a2c37c5e6094ec
Christopher Wenceslaus <topher.wenceslaus@gmail.com>
2018-09-26 03:31:17 +0530

    ee

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,4 +1,3 @@
-import store from '../store/index'
 export const transfer_balance =(data)=>({
 	type: "TRANSFER_BALANCE",
 	data: data
@@ -6,22 +5,4 @@ export const transfer_balance =(data)=>({
 
 export const initial_balance = ()=>(
 	{type: "INITIAL_BALANCE"}
-	)
-
-export const thunk_action_creator = (user)=>{
-
-		store.dispatch(initial_balance());
-	
-		return (dispatch , getState)=>{
-			return fetch(`https://codepen.io/andismith/pen/oqzxyP.js`)
-				   .then(data => data.json())
-				   .then(data=>{
-					   if(data.message === ' Not Found') {
-						   throw new Error('balance not found')
-					   } else {
-						   dispatch(initial_balance(data))
-					   }
-				   })
-				   .catch(err => dispatch(initial_balance()))
-		} 
-	}
+)
